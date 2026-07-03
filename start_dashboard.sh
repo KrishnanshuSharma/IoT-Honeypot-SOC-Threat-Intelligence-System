e9f7c0051f9e33d1ae56a0e7125d2a7a8bfcf346
KrishnanshuSharma <132912599+KrishnanshuSharma@users.noreply.github.com>
2026-07-03 13:39:02 +0530

    fix: remove unnecessary files

--- start_dashboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Export the key to the environment, then run the dashboard
-export ABUSE_IPDB_KEY="47dfcec04fee80c2115634c703d04cf2dc68ee623b4b2feb4ea7310bee92dd693bd3e9755f6379c7"
-sudo -E python3 /home/adminn/iot_project/dashboard.py
